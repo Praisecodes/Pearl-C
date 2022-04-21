294af7e6e682558369a5ae96e83ef825d6ca8a4e
Praisecodes <cmancoder@gmail.com>
2022-04-22 00:45:56 +0100

    First Commit Today, Did A Lot Of Changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
 const newest = document.querySelector(".newest");
 const OpenMobileNav = document.querySelector(".OpenMobileNav");
 const newest_list = document.querySelector(".newest-list");
+const contact = document.querySelector(".contact");
+const ContactDiv = document.querySelector(".contactDiv");
+const subscribeForm = document.querySelector(".subscribeForm");
+const email = document.querySelector(".email");
+const submitEmail = document.querySelector(".submitEmail");
 
+contact.addEventListener("click", ()=>{
+    ContactDiv.scrollIntoView({
+        behavior: "smooth"
+    });
+});
 
+subscribeForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    if(!(email.value == "")){
+        window.location = "./read/";
+        submitEmail.disabled = true;
+    }
+    else{
+        console.log("No No!!");
+    }
+});
