8ad9afaa87b8e641736fe29255ecd7933d3ae545
Praisecodes <cmancoder@gmail.com>
2022-04-27 17:20:46 +0100

    ...

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -14,7 +14,7 @@
     // $dotenv = Dotenv::createImmutable(__DIR__);
     // $dotenv->load();
 
-    $emailPassword = parse_url(getenv(base64_encode("EMAIL_PASSWORD")));
+    $emailPassword = parse_url(getenv("EMAIL_PASSWORD"));
 
     $ContentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : "Not Set";
 
@@ -30,7 +30,7 @@
         $mail->Host = "smtp.gmail.com";
         $mail->SMTPAuth = true;
         $mail->Username = "praisetesting24@gmail.com";
-        $mail->Password = $emailPassword;
+        $mail->Password = getenv("EMAIL_PASSWORD");
         $mail->Port = 465;
         $mail->SMTPSecure = "ssl";
 
