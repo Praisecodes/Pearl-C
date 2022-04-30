f4ae5aec0f2e1ca10b9c9d05ef599f782a01726f
Praisecodes <cmancoder@gmail.com>
2022-04-30 10:38:29 +0100

    Fixing figuresg

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,5 +1,24 @@
 const blogRedirect = document.querySelector(".blogRedirect");
+const OpenMobileSideBar = document.querySelector(".workarea-nav-open");
+const mobileSideBarContainer = document.querySelector(".mobileSideBarContainer");
+const mobileSideBar = document.querySelector(".mobileSideBar");
 
 blogRedirect.addEventListener("click", (e)=>{
     window.location = "../";
 });
+
+OpenMobileSideBar.addEventListener('click', (e)=>{
+    mobileSideBarContainer.style.display = "block";
+    setTimeout(() => {
+        mobileSideBar.classList.add("showRight");
+        mobileSideBar.classList.remove("showLeft");
+    }, 0);
+});
+
+mobileSideBarContainer.addEventListener('click', (e)=>{
+    mobileSideBar.classList.add("showLeft");
+    mobileSideBar.classList.remove("showRight");
+    setTimeout(() => {
+        mobileSideBarContainer.style.display = "none";
+    }, 500);
+})
