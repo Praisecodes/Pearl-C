d07de9643acdc34464c709879107cce2e6145393
Praisecodes <cmancoder@gmail.com>
2022-05-01 00:40:44 +0100

    Doing some frontend functionality"

--- scripts/admin.js
+++ scripts/admin.js
@@ -9,9 +9,14 @@ const NewPostHeaderContainer = document.querySelector(".NewPostHeaderContainer")
 const NewPostHeader = document.querySelector(".NewPostHeader");
 const addPost = document.querySelector(".addPost");
 const mobileAddPost = document.querySelector(".mobileAddPost");
+const CreatePostBtn = document.querySelector(".CreatePost");
+const TitleInput = document.querySelector(".TitleInput");
+const categoryInput = document.querySelector(".categoryInput");
+const titleBar = document.querySelector(".titleBar");
+const editBtn = document.querySelector(".edit");
 
 //Variables to store the title and category
-let title, category;
+let title = "", category = "";
 
 blogRedirect.addEventListener("click", (e)=>{
     window.location = "../";
@@ -34,6 +39,10 @@ mobileSideBarContainer.addEventListener('click', (e)=>{
 });
 
 const closeNewPost = () =>{
+    if(!(TitleInput.value == "" && categoryInput == "")){
+        TitleInput.value = "";
+        categoryInput.value = "";
+    }
     NewPostHeader.classList.add("shrink");
     NewPostHeader.classList.remove("fullscale");
     setTimeout(() => {
@@ -49,6 +58,17 @@ const addNewPost = () => {
     }, 0);
 }
 
+const CreatePost = () =>{
+    title = TitleInput.value.toUpperCase();
+    category = categoryInput.value.toUpperCase();
+
+}
+
 closeNewPostHeader.addEventListener('click', closeNewPost);
 addPost.addEventListener('click', addNewPost);
 mobileAddPost.addEventListener('click', addNewPost);
+editBtn.addEventListener('click', ()=>{
+    TitleInput.value = title;
+    categoryInput.value = category;
+    addNewPost();
+})
