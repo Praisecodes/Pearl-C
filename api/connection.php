3a0a4af2261a73318ae8ff69ae9e66d96c7ccdac
Praisecodes <cmancoder@gmail.com>
2022-05-14 18:46:09 +0100

    Update: Working on side bar population

--- api/connection.php
+++ api/connection.php
@@ -2,15 +2,15 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
 
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"],1);
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
 
-    // $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-    $conn = new mysqli("localhost", "root", "", "pearlcdb");
+    // $conn = new mysqli("localhost", "root", "", "pearlcdb");
 
     if($conn->connect_error){
         exit(json_encode([
